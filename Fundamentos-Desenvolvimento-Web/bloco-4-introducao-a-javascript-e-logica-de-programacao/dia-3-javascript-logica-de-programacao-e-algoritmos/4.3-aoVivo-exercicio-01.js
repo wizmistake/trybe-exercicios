d52e1b75d46409fa070373da94b600d47c3e3887
wizmistake <akbar63@gmail.com>
2022-02-19 18:28:50 -0300

    4.3-aoVivo-exercicio-01

---
+++ Fundamentos-Desenvolvimento-Web/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/4.3-aoVivo-exercicio-01.js
@@ -0,0 +1,11 @@
+/*
+1. Faça um algoritmo que calcule a soma de 1 a 50 usando a estrutura "for" e retorne no formato:
+*/
+
+let soma = 0;
+for (let n = 0; n <= 50; n += 1){
+  soma += n;
+  //console.log(n);
+  //console.log(soma);
+}
+console.log("A soma dos 50 termos é =", soma);
