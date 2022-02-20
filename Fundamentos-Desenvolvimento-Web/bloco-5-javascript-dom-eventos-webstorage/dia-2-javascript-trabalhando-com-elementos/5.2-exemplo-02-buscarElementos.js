10c986874b007e7490ab0f69707239e45721095a
wizmistake <akbar63@gmail.com>
2022-02-20 18:02:38 -0300

    5.2-exemplo-02-questao-08-ok

--- Fundamentos-Desenvolvimento-Web/bloco-5-javascript-dom-eventos-webstorage/dia-2-javascript-trabalhando-com-elementos/5.2-exemplo-02-buscarElementos.js
+++ Fundamentos-Desenvolvimento-Web/bloco-5-javascript-dom-eventos-webstorage/dia-2-javascript-trabalhando-com-elementos/5.2-exemplo-02-buscarElementos.js
@@ -33,3 +33,6 @@ console.log('6.',document.getElementById("elementoOndeVoceEsta").nextSibling);
 
 // Questão 7 - ok
 console.log('7.',document.getElementById("elementoOndeVoceEsta").nextElementSibling);
+
+// Questão 8 - 
+console.log('8.',document.getElementById("pai").lastElementChild.previousElementSibling);
