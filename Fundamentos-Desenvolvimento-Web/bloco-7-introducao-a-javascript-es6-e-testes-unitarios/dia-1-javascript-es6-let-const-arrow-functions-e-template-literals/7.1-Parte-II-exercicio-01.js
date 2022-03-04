7393b11351bf6a382106430d13eaceea933b1455
wizmistake <akbar63@gmail.com>
2022-03-04 19:31:49 -0300

    questao 4 - funcao 1 - ok

--- Fundamentos-Desenvolvimento-Web/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/7.1-Parte-II-exercicio-01.js
+++ Fundamentos-Desenvolvimento-Web/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/7.1-Parte-II-exercicio-01.js
@@ -1,8 +1,6 @@
 /*
  Abaixo, você verá algumas especificações de algoritmos para desenvolver. É fundamental que você utilize o que aprendeu sobre let , const , arrow functions , template literals e ternary operator .
-
   1. Crie uma função que receba um número e retorne seu fatorial.
-
     Na matemática, o fatorial de um número não negativo N , com a notação N! , é o produto de todos os inteiros menores ou iguais a N . Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
     Bônus (opcional): tente fazer o mesmo exercício de forma recursiva . Spoiler: É possível resolver com uma linha usando ternary operator .
 */
@@ -25,9 +23,45 @@ console.log(fatorial(7));
 const maiorPalavra = (frase) => {
   const palavras = frase.split(' ');
   let maiorPalavra = '';
-  for (let i = 0; i <= palavras.length; i += 1) {
-    maiorPalavra.length < palavras[i].length ? maiorPalavra = palavras[i] : false;
+  for (let i = 0; i < palavras.length; i += 1) {
+    maiorPalavra.length <= palavras[i].length ? maiorPalavra = palavras[i] : false;
   }
   return maiorPalavra;
 }
 console.log(maiorPalavra('Crie uma função que receba uma frase e retorne qual a maior palavra'));
+
+
+/*
+  3. Crie uma página que contenha:
+
+    Um botão ao qual será associado um event listener ;
+    Uma variável clickCount no arquivo JavaScript que acumule o número de clicks no botão;
+    Um campo no HTML que vá atualizando a quantidade de clicks no botão conforme a variável clickCount é atualizada.
+*/
+
+// let botao = document.getElementById('botao');
+// const clicks = (event) => {
+ 
+// }
+// botao.addEventListener('click', clicks);
+// ##### Depois eu termino a questão 3 #####
+
+
+/*
+  4. Crie um código JavaScript com a seguinte especificação:
+    Não se esqueça de usar template literals
+
+  Função 1 : Escreva uma função que vai receber uma string como parâmetro. Sua função deverá procurar pela letra x em uma string qualquer que você determinar e substituir pela string que você passou como parâmetro. Sua função deve retornar essa nova string .
+    Exemplo:
+        String determinada: "Tryber x aqui!"
+        Parâmetro: "Bebeto"
+        Retorno: "Tryber Bebeto aqui!"
+    Um array com escopo global, que é o escopo do arquivo JS , nesse caso, contendo cinco strings com suas principais skills .
+*/
+
+const substituirX = (string) => {
+  let parametro = 'Bebeto';
+  let resultado = string.replace(/x/gi, parametro); // "g" substitui N caracteres iguais. "i" ignora maiúsculo/minúsculo.
+  return resultado;
+}
+console.log(substituirX('Tryber x aqui!'));
