8de45162368e1f8d50301a8f0e4ac03bf8f7478c
wizmistake <akbar63@gmail.com>
2022-05-11 17:27:54 -0300

    8.1-exercicio-questao3-ok

--- Fundamentos-Desenvolvimento-Web/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/8.1-exercicio.js
+++ Fundamentos-Desenvolvimento-Web/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/8.1-exercicio.js
@@ -43,3 +43,33 @@ const sorteio = (numero, verificador) => {
 }
 
 sorteio (3, gerarAleatorioEntre(1,5));
+
+
+/*
+   3 - Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+
+   - Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.
+*/
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'N.A'];
+
+const pontuacao = (gabarito, resposta) => {
+  if (gabarito === resposta) {
+    return +1;
+  }
+  if (resposta === "N.A") {
+    return 0;
+  }
+  return -0.5;
+}
+
+const notaProva = (gabarito, resposta, verificador) => {
+  let nota = 0;
+  for (let i = 0; i < gabarito.length; i += 1) {
+    nota += verificador(gabarito[i], resposta[i]);
+  }
+  return `Pontuação Final: ${nota} pontos.`
+};
+
+console.log(notaProva(RIGHT_ANSWERS, STUDENT_ANSWERS, pontuacao));
