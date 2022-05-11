8e0fd8e33cfe33a3c1b675627c77589f6c3075a7
wizmistake <akbar63@gmail.com>
2022-05-11 16:56:11 -0300

    8.1-exercicio-questao2-ok

--- Fundamentos-Desenvolvimento-Web/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/8.1-exercicio.js
+++ Fundamentos-Desenvolvimento-Web/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/8.1-exercicio.js
@@ -27,3 +27,19 @@ const newEmployees = (nome) => {
 };
 
 console.log(newEmployees(employeesId));
+
+/*
+  2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+*/
+
+const gerarAleatorioEntre = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const sorteio = (numero, verificador) => {
+  numero === verificador ? console.log("Parabéns, você ganhou!") : console.log("Tente Novamente.");
+}
+
+sorteio (3, gerarAleatorioEntre(1,5));
